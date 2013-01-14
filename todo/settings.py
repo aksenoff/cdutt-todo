9d062b0cc0c4db4c704fc8246f7a0b4431f8ea75
aksenoff.a <aksenoff.a@85212a02-8378-b5ad-cac3-82525f25b3dc>
2013-01-14 01:53:46 +0000

    Path to project modified
    git-svn-id: https://cdutt-todo.googlecode.com/svn/trunk@3 85212a02-8378-b5ad-cac3-82525f25b3dc

--- todo/settings.py
+++ todo/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Programming\\todo\\db\\db.db3',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:\\Programming\\cdutt-todo\\db\\db.db3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -103,7 +103,7 @@ ROOT_URLCONF = 'todo.urls'
 WSGI_APPLICATION = 'todo.wsgi.application'
 
 TEMPLATE_DIRS = (
-    'C:/Programming/todo/templates'
+    'C:/Programming/cdutt-todo/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
